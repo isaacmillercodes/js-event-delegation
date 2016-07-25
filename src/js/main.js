9dff857ead30da89d20a45bb3596922f60807447
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-25 10:07:35 -0600

    adding to github repo

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // ** globals ** //
 
-var seed = [
+var seedData = [
   'Sing happy birthday',
   'Wash the car',
   'Watch the Avalanche win',
@@ -10,7 +10,28 @@ var seed = [
 // ** dom manipulation ** //
 
 $(document).on('ready', function() {
-
+  seedData.forEach(function(seed) {
+    addToDoToDom(seed);
+  })
 });
 
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  var $newToDo = $('#todoInput');
+
+  addToDoToDom($newToDo.val())
+
+  $newToDo.val('');
+})
+
 // ** helper functions ** //
+
+function addToDoToDom(seed) {
+  $('#all-todos').append('<li>' + seed + '</li>')
+}
+
+$('ol').on('click', 'li', function() {
+  console.log('hey buddy this works');
+  $(this).remove();
+})
